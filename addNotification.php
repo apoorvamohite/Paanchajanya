11fa00c469ed7978dfbd64bde308b46824464d7b
mrunalambekar <45233550+mrunalambekar@users.noreply.github.com>
2018-12-29 09:54:19 +0530

    Add files via upload

--- addNotification.php
+++ addNotification.php
@@ -13,10 +13,42 @@
     	die("Connection failed: " . mysqli_connect_error());
 	}
 	mysqli_select_db($con, $dbname);
+	
+	
+	
+		$desc = $_POST['description'];
+		
+		$file = $_FILES['file'];
+		
+		$fileName = $_FILES['file']['name'];
+		$fileTmpName = $_FILES['file']['tmp_name'];
+		$fileSize = $_FILES['file']['size'];
+		$fileError = $_FILES['file']['error'];
+		$fileType = $_FILES['file']['type'];
+		
+		$fileExt = explode('.',$fileName );
+		$fileActualExt = strtolower(end($fileExt));
+		
+		$allowed = array('pdf', 'doc', 'docx', 'xls', 'xlsx', 'txt');
+		
+		if(in_array($fileActualExt, $allowed)){
+			if($fileError === 0){
+				if($fileSize < 100000000){
+					$fileNameNew = uniqid('',true).'.'.$fileActualExt;
+					$fileDestination = 'upload/'.$fileNameNew;
+					move_uploaded_file($fileTmpName, $fileDestination);
+				}else{
+					echo "Your file is too big!";
+				}
+			}else{
+				echo "There was an error uploading this file!";
+			}
+		}else{
+			echo "You cannot upload files of this type!";
+		}
 
-$desc = $_POST['description'];
-
-$sql = "INSERT INTO notifications(description) VALUES('$desc')";
-mysqli_query($con, $sql);
-header("Location: adminHome.php?success=true");
+    $sql = "INSERT INTO notifiacations(description, file) VALUES('$desc', '$fileNameNew')";
+    mysqli_query($con, $sql);
+	header("Location: adminHome.php?success=true");
+	
 ?>
