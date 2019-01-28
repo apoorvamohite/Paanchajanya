d3a174ee2a9d0ee1bf2751e986e8e33b99973f13
mrunalambekar <45233550+mrunalambekar@users.noreply.github.com>
2019-01-28 23:05:11 +0530

    Add files via upload

---
+++ ProjectExpo.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Now UI Kit by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="./assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="index-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="#" data-placement="bottom" target="_blank">
+          Paanchajanya 2k19
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar top-bar"></span>
+          <span class="navbar-toggler-bar middle-bar"></span>
+          <span class="navbar-toggler-bar bottom-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+              <p>Schedule</p>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink1" data-toggle="dropdown">
+              <i class="now-ui-icons design_app"></i>
+              <p>Events</p>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink1">
+              <a class="dropdown-item" href="./index.html">
+                <i class="now-ui-icons business_chart-pie-36"></i> Event 1
+              </a>
+              <a class="dropdown-item" target="_blank" href="https://demos.creative-tim.com/now-ui-kit/docs/1.0/getting-started/introduction.html">
+                <i class="now-ui-icons design_bullet-list-67"></i> Event 2
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-neutral" href="register.html" target="_blank">
+              <i class="now-ui-icons arrows-1_share-66"></i>
+              <p>Register</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+              <i class="fab fa-twitter"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+              <p class="d-lg-none d-xl-none">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+              <i class="fab fa-instagram"></i>
+              <p class="d-lg-none d-xl-none">Instagram</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="page-header clear-filter" filter-color="orange">
+      <div class="page-header-image" data-parallax="true" style="background-image:url('./assets/img/header.jpg');">
+      </div>
+      <div class="container">
+        <!--<div>
+          <h2 class="title" align="center">Register</h2>
+        <div>-->
+            <div class="container">
+              <div class="row">
+                <div class="card card-signup" data-background-color="orange">
+                  <form class="form" method="POST" action="Event7.php">
+                    <div class="card-header text-center">
+                      <h3 class="card-title title-up">Team formation</h3>
+                    </div>
+                      <label>Team member 1</label>
+                    <div class="card-body">
+                      <div class="input-group no-border">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="now-ui-icons users_circle-08"></i>
+                          </span>
+                            
+                        </div>
+                        <input type="text" class="form-control" placeholder="Order ID" name="invoice1" />
+                      </div>
+                      <br><br>
+                      <!-- If you want to add a checkbox to this form, uncomment this code -->
+                      <!--<div class="checkbox">
+                        <input id="checkboxSignup" type="checkbox" name="ieeemember" />
+                        <label for="checkboxSignup">
+                          I am IEEE member
+                        </label>
+                      </div> -->
+                     <label>Team member 2</label>
+                    
+                      <div class="input-group no-border">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="now-ui-icons users_circle-08"></i>
+                          </span>
+                        </div>
+                        <input type="text" placeholder="Order ID" class="form-control" name="invoice2" />
+                      </div>
+					  </br></br>
+					  
+					  <label>Team member 3</label>
+                    <div class="card-body">
+                      <div class="input-group no-border">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="now-ui-icons users_circle-08"></i>
+                          </span>
+                            
+                        </div>
+                        <input type="text" class="form-control" placeholder="Order ID" name="invoice1" />
+                      </div>
+                      <br><br>
+                      <!-- If you want to add a checkbox to this form, uncomment this code -->
+                      <!--<div class="checkbox">
+                        <input id="checkboxSignup" type="checkbox" name="ieeemember" />
+                        <label for="checkboxSignup">
+                          I am IEEE member
+                        </label>
+                      </div> -->
+                     <label>Team member 4</label>
+                    
+                      <div class="input-group no-border">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">
+                            <i class="now-ui-icons users_circle-08"></i>
+                          </span>
+                        </div>
+                        <input type="text" placeholder="Order ID" class="form-control" name="invoice2" />
+                      </div>
+					  
+					 
+                    </div>
+                    <div class="card-footer text-center">
+                      <input type="submit" class="btn btn-neutral btn-round btn-lg" value="Submit">
+                        <div class="h3">
+                         <?php
+                        if(isset($_GET['msg']))
+                        {
+                            echo $_GET['msg'];
+                            unset($_GET['msg']);
+                        }
+                        ?>
+                        </div>        
+                    </div>
+                     
+                  </form>
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--   Core JS Files   -->
+      <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+      <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+      <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+      <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+      <script src="./assets/js/plugins/bootstrap-switch.js"></script>
+      <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+      <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+      <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+      <script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+      <!--  Google Maps Plugin    -->
+      <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+      <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+      <script src="./assets/js/now-ui-kit.js?v=1.2.0" type="text/javascript"></script>
+      <script>
+        $(document).ready(function() {
+      // the body of this function is in assets/js/now-ui-kit.js
+      nowuiKit.initSliders();
+    });
+
+        function scrollToDownload() {
+
+          if ($('.section-download').length != 0) {
+            $("html, body").animate({
+              scrollTop: $('.section-download').offset().top
+            }, 1000);
+          }
+        }
+      </script>
+    </body>
+
+    </html>
